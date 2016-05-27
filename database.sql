d3b1b85a07d52f14962ddfadccfd4023cf7ec3fe
simonvasseur <Simonvasseur91550@gmail.com>
2016-05-27 21:57:33 +0200

    db non testee
    db non testee

--- database.sql
+++ database.sql
@@ -1,28 +1,14 @@
 CREATE TABLE User(
-    idUser INT PRIMARY KEY AUTO_INCREMENT,
-    pseudo VARCHAR(20) UNIQUE NOT NULL,
+    pseudo VARCHAR(20) PRIMARY KEY UNIQUE NOT NULL,
     prenom VARCHAR(20) NOT NULL,
     nom VARCHAR(20) NOT NULL,
     mail VARCHAR(50) UNIQUE NOT NULL,
-    motPasse VARCHAR(50) NOT NULL     
+    motPasse VARCHAR(25) NOT NULL     
 );
 
-CREATE TABLE Album(
-    idAlbum INT PRIMARY KEY AUTO_INCREMENT,
-    nomAlbum VARCHAR(30) NOT NULL,
-    artiste VARCHAR(30) NOT NULL,
-    sortieAlbum DATE,
-    lienYoutube VARCHAR(100),
-    note INT    
-);
-
-CREATE TABLE Chanson(
-    idChanson INT PRIMARY KEY AUTO_INCREMENT,
-    nomChanson VARCHAR(30) NOT NULL,
-    dureeChanson FLOAT,
-    artiste VARCHAR(30) NOT NULL,
-    Album VARCHAR(30) NOT NULL,
-    lienYoutube VARCHAR(100)    
+CREATE TABLE Administrateur(
+    idAdmin INT NOT NULL,
+    FOREIGN KEY pseudo REFERENCES User(pseudo)
 );
 
 CREATE TABLE Artiste(
@@ -34,10 +20,35 @@ CREATE TABLE Artiste(
     lienWiki VARCHAR(100)
 );
 
-CREATE TABLE Notation(
-    idNotation INT PRIMARY KEY AUTO_INCREMENT,
-    idArtiste INT,
-    idAlbum INT,
+CREATE TABLE Album(
+    idAlbum INT PRIMARY KEY AUTO_INCREMENT,
+    nomAlbum VARCHAR(30) NOT NULL,
+    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) NOT NULL,
+    sortieAlbum DATE,
+    lienYoutube VARCHAR(100),
+    note INT
+);
+
+CREATE TABLE Chanson(
+    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) PRIMARY KEY NOT NULL,
+    nomChanson VARCHAR(30) PRIMARY KEY NOT NULL,
+    dureeChanson FLOAT,
+    FOREIGN KEY album REFERENCES Album(idAlbum) NOT NULL,
+    lienYoutube VARCHAR(100)    
+);
+
+CREATE TABLE Commentaire(
+    idCommentaire INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY pseudo REFERENCES User(pseudo) NOT NULL,
+    FOREIGN KEY Album REFERENCES Album(idAlbum) NOT NULL,
     note INT NOT NULL,
     commentaire VARCHAR(500)  
 );
+
+CREATE TABLE Groupe(
+    idGroupe INT PRIMARY KEY AUTO_INCREMENT,
+    nomGroupe VARCHAR(30),
+    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) NOT NULL,
+)
+
+
