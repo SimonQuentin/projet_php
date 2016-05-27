fc8e53e4f6844e7ee2266db6b93b9db4065aa3fd
simonvasseur <Simonvasseur91550@gmail.com>
2016-05-27 22:09:25 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -8,7 +8,8 @@ CREATE TABLE User(
 
 CREATE TABLE Administrateur(
     idAdmin INT NOT NULL,
-    FOREIGN KEY pseudo REFERENCES User(pseudo)
+    pseudo VARCHAR(20) NOT NULL,
+    FOREIGN KEY (pseudo) REFERENCES User(pseudo)
 );
 
 CREATE TABLE Artiste(
@@ -23,24 +24,29 @@ CREATE TABLE Artiste(
 CREATE TABLE Album(
     idAlbum INT PRIMARY KEY AUTO_INCREMENT,
     nomAlbum VARCHAR(30) NOT NULL,
-    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) NOT NULL,
+    artiste VARCHAR(30) NOT NULL,
+    FOREIGN KEY (artiste) REFERENCES Artiste(idArtiste),
     sortieAlbum DATE,
     lienYoutube VARCHAR(100),
     note INT
 );
 
 CREATE TABLE Chanson(
-    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) PRIMARY KEY NOT NULL,
+    artiste VARCHAR(30) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (artiste) REFERENCES Artiste(idArtiste),
     nomChanson VARCHAR(30) PRIMARY KEY NOT NULL,
     dureeChanson FLOAT,
-    FOREIGN KEY album REFERENCES Album(idAlbum) NOT NULL,
+    album VARCHAR(30),
+    FOREIGN KEY (album) REFERENCES Album(idAlbum),
     lienYoutube VARCHAR(100)    
 );
 
 CREATE TABLE Commentaire(
     idCommentaire INT PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY pseudo REFERENCES User(pseudo) NOT NULL,
-    FOREIGN KEY Album REFERENCES Album(idAlbum) NOT NULL,
+    pseudo VARCHAR(20) NOT NULL,
+    FOREIGN KEY (pseudo) REFERENCES User(pseudo),
+    album VARCHAR(30) NOT NULL,
+    FOREIGN KEY (album) REFERENCES Album(idAlbum),
     note INT NOT NULL,
     commentaire VARCHAR(500)  
 );
@@ -48,7 +54,8 @@ CREATE TABLE Commentaire(
 CREATE TABLE Groupe(
     idGroupe INT PRIMARY KEY AUTO_INCREMENT,
     nomGroupe VARCHAR(30),
-    FOREIGN KEY artiste REFERENCES Artiste(idArtiste) NOT NULL,
-)
+    artiste VARCHAR(30) NOT NULL,
+    FOREIGN KEY (artiste) REFERENCES Artiste(idArtiste),
+);
 
 
